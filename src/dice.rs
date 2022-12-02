c9337e05e15def29a6627ac0dd8d50de12870485
Simon Warta <simon@warta.it>
2022-12-02 20:36:34 +0100

    Add roll_dice function

---
+++ src/dice.rs
@@ -0,0 +1,78 @@
+use crate::int_in_range;
+
+/// Returns a number from 1-6.
+///
+/// # Example
+///
+/// Single dice
+///
+/// ```
+/// use nois::roll_dice;
+///
+/// let randomness: [u8; 32]
+///     = hex::decode("9e8e26615f51552aa3b18b6f0bcf0dae5afbe30321e8d7ea7fa51ebeb1d8fe62").unwrap().try_into().unwrap();
+///
+/// let number = roll_dice(randomness);
+/// assert_eq!(number, 2);
+/// ```
+///
+/// Double dice
+///
+/// ```
+/// use nois::{roll_dice, sub_randomness};
+///
+/// let randomness: [u8; 32]
+///     = hex::decode("e2ee31c20afbb20530f6a18d5d96fb20f766fe11799d3611b04bf9edbd2cffcb").unwrap().try_into().unwrap();
+/// let mut provider = sub_randomness(randomness);
+///
+/// let number1 = roll_dice(provider.provide());
+/// assert_eq!(number1, 3);
+///
+/// let number2 = roll_dice(provider.provide());
+/// assert_eq!(number2, 6);
+/// ```
+pub fn roll_dice(randomness: [u8; 32]) -> u8 {
+    int_in_range(randomness, 1..=6)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::sub_randomness;
+    use hex_literal::hex;
+    use std::collections::HashMap;
+
+    #[test]
+    fn roll_dice_works() {
+        let randomness: [u8; 32] =
+            hex!("9e8e26615f51552aa3b18b6f0bcf0dae5afbe30321e8d7ea7fa51ebeb1d8fe62");
+        let number = roll_dice(randomness);
+        assert_eq!(number, 2);
+    }
+
+    #[test]
+    fn roll_dice_is_uniformly_distributes() {
+        let randomness: [u8; 32] =
+            hex!("5ec7020fe74dff44d50b255c1a680c362dc83de69bd3c865e0ef5f914bea6f7b");
+        let mut results = HashMap::<u8, usize>::new();
+        for sub_randomness in sub_randomness(randomness).take(600_000) {
+            let number = roll_dice(sub_randomness);
+            let current = results.get(&number).copied().unwrap_or_default();
+            results.insert(number, current + 1);
+        }
+        let ones = results.get(&1).copied().unwrap_or_default();
+        let twos = results.get(&2).copied().unwrap_or_default();
+        let threes = results.get(&3).copied().unwrap_or_default();
+        let fours = results.get(&4).copied().unwrap_or_default();
+        let fives = results.get(&5).copied().unwrap_or_default();
+        let sixes = results.get(&6).copied().unwrap_or_default();
+        // println!("{ones} {twos} {threes} {fours} {fives} {sixes}");
+        assert!(ones > 99_000 && ones < 101_000);
+        assert!(twos > 99_000 && twos < 101_000);
+        assert!(threes > 99_000 && threes < 101_000);
+        assert!(fours > 99_000 && fours < 101_000);
+        assert!(fives > 99_000 && fives < 101_000);
+        assert!(sixes > 99_000 && sixes < 101_000);
+        assert_eq!(results.values().sum::<usize>(), 600_000);
+    }
+}
--- src/js_wrapper.rs
+++ src/js_wrapper.rs
@@ -29,11 +29,12 @@ pub fn _coinflip_js(randomness: &str) -> String {
     side.to_string()
 }
 
+// Returns a value from 1 to 6 (inclusive)
 #[wasm_bindgen]
-pub fn _int_in_range_single_dice_js(randomness: &str) -> u8 {
+pub fn _roll_dice_js(randomness: &str) -> u8 {
     let hex_randomness = hex::decode(randomness).unwrap();
     let hex_randomness_array = _cast_vec_to_array(hex_randomness);
-    int_in_range(hex_randomness_array, 1..6)
+    roll_dice(hex_randomness_array)
 }
 
 #[wasm_bindgen]
