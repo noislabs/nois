205418aa5c01c5150e362845485aedc61a19acee
Kais <baccour.kais@yahoo.fr>
2023-07-21 19:55:09 +0200

    add-possibility-to-incentivise-relayer-on-consumer-chain

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,5 +1,5 @@
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{Addr, HexBinary, Timestamp};
+use cosmwasm_std::{HexBinary, Timestamp};
 
 /// Max length that the job ID is allowed to have (in bytes)
 ///
